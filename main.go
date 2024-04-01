4e0ab0b2ccccc67e2f53ebe8d675ae981612286b
SeungKang <13smk@queensu.ca>
2024-04-01 12:24:22 -0400

    systray: first implementation

--- main.go
+++ main.go
@@ -6,36 +6,113 @@ import (
 	"fmt"
 	"github.com/SeungKang/speedometer/internal/appconfig"
 	"github.com/SeungKang/speedometer/internal/gamectl"
+	"github.com/getlantern/systray"
 	"github.com/stephen-fox/user32util"
 	_ "image/png"
-	"log"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
+	"time"
 )
 
-func main() {
-	log.SetFlags(0)
+const appName = "buh"
 
-	err := mainWithError()
-	if err != nil {
-		log.Fatalln("error:", err)
+func main() {
+	a := &app{}
+	systray.Run(a.ready, a.exit)
+}
+
+type app struct {
+	status      *systray.MenuItem
+	statusChild *systray.MenuItem
+}
+
+func (o *app) ready() {
+	systray.SetTitle(appName)
+
+	o.status = systray.AddMenuItem("Status", "Application status")
+	o.statusChild = o.status.AddSubMenuItem("", "")
+	o.statusChild.Hide()
+
+	quit := systray.AddMenuItem("Quit", "Quit the application")
+	ctx, cancelFn := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		select {
+		case <-quit.ClickedCh:
+		case <-ctx.Done():
+		}
+
+		cancelFn()
+		o.exit()
+	}()
+
+	go o.loop(ctx)
+}
+
+func (o *app) loop(ctx context.Context) {
+	for {
+		var cancelFn func()
+		ctx, cancelFn = context.WithCancel(ctx)
+		defer cancelFn()
+
+		gameErrors, err := startApp(ctx)
+		if err != nil {
+			goto onError
+		}
+
+		o.status.SetTitle("Status: running")
+		o.statusChild.Hide()
+
+		select {
+		case <-ctx.Done():
+		case err = <-gameErrors:
+		}
+
+	onError:
+		cancelFn()
+
+		o.status.SetTitle("Status: error")
+		o.statusChild.SetTitle(err.Error())
+		o.statusChild.Show()
+
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(5 * time.Second):
+			continue
+		}
 	}
 }
 
-func mainWithError() error {
+func (o *app) exit() {
+	os.Exit(0)
+}
+
+func startApp(ctx context.Context) (<-chan error, error) {
 	user32, err := user32util.LoadUser32DLL()
 	if err != nil {
-		return fmt.Errorf("failed to load user32.dll - %s", err.Error())
+		return nil, fmt.Errorf("failed to load user32.dll - %s", err.Error())
 	}
 
-	config, err := appconfig.Parse(os.Stdin)
+	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		return fmt.Errorf("failed to parse config - %w", err)
+		return nil, fmt.Errorf("failed to get user home dir - %w", err)
 	}
 
-	ctx, cancelFn := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer cancelFn()
+	configPath := filepath.Join(homeDir, "."+appName, "config.conf")
+	configFile, err := os.Open(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open config file - %w", err)
+	}
+	defer configFile.Close()
+
+	config, err := appconfig.Parse(configFile)
+	configFile.Close()
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse config - %w", err)
+	}
 
 	gameRoutinesExited := make(chan error, len(config.Games))
 	for _, game := range config.Games {
@@ -56,10 +133,5 @@ func mainWithError() error {
 		}()
 	}
 
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case err := <-gameRoutinesExited:
-		return err
-	}
+	return gameRoutinesExited, nil
 }
