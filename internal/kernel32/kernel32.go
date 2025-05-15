771bed8f6f48c413e92b5c6a5a4f46789a6c5608
SeungKang <13smk@queensu.ca>
2025-05-14 20:58:41 -0400

    kernel32: not hardcoded module limit
    and properly list 64 bit process modules

--- internal/kernel32/kernel32.go
+++ internal/kernel32/kernel32.go
@@ -1,12 +1,24 @@
 package kernel32
 
 import (
+	"errors"
 	"fmt"
 	"path/filepath"
 	"syscall"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
+)
+
+// Various dwFilterFlag values.
+//
+// See also:
+// https://learn.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-enumprocessmodulesex
+const (
+	ListModulesDefault = 0x00
+	ListModules32Bit   = 0x01
+	ListModules64Bit   = 0x02
+	ListModulesAll     = 0x03
 )
 
 var (
@@ -31,37 +43,75 @@ type Module struct {
 	Filepath string
 	Filename string
 	BaseAddr uintptr
+	EndAddr  uintptr
+	Size     uint64
 }
 
-// ProcessModules returns the target process's modules
+// ProcessModules returns the target process's modules.
 //
-// the process handle must be opened with
-// windows.PROCESS_VM_READ | windows.PROCESS_QUERY_INFORMATION
+// The process handle must be opened with:
+//
+//	PROCESS_VM_READ | PROCESS_QUERY_INFORMATION
 func ProcessModules(processHandle syscall.Handle) ([]Module, error) {
-	// TODO: handle more than 1024 (lookup maximum file handles and use that)
-	moduleHandles := make([]syscall.Handle, 1024)
-	numModuleHandles, err := EnumProcessModulesEx(processHandle, moduleHandles)
+	var modules []Module
+
+	err := IterProcessModules(processHandle, func(i int, total uint, mod Module) error {
+		if len(modules) == 0 {
+			modules = make([]Module, total)
+		}
+
+		modules[i] = mod
+
+		return nil
+	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to enum process modules - %w", err)
-	}
-	defer func() {
-		for _, handle := range moduleHandles[0:numModuleHandles] {
-			syscall.CloseHandle(handle)
-		}
-	}()
-
-	modules := make([]Module, numModuleHandles)
-	for i, moduleHandle := range moduleHandles[0:numModuleHandles] {
-		module, err := lookupModuleInfo(processHandle, moduleHandle)
-		if err != nil {
-			return nil, fmt.Errorf("failed to lookup module info handle: %v - %w",
-				moduleHandle, err)
-		}
-
-		modules[i] = module
+		return nil, err
 	}
 
 	return modules, nil
+}
+
+// IterProcessModules calls iterFn for each of the target process's modules.
+//
+// The process handle must be opened with:
+//
+//	PROCESS_VM_READ | PROCESS_QUERY_INFORMATION
+func IterProcessModules(processHandle syscall.Handle, iterFn func(i int, total uint, mod Module) error) error {
+	totalHandles, err := TotalEnumProcessModulesEx(processHandle, ListModulesAll)
+	if err != nil {
+		return fmt.Errorf("failed to get total number of process handles - %w", err)
+	}
+
+	if totalHandles == 0 {
+		return errors.New("total number of process handles is zero (this should never happen)")
+	}
+
+	moduleHandles := make([]syscall.Handle, totalHandles)
+
+	actualHandlesReturned, err := EnumProcessModulesEx(
+		processHandle,
+		moduleHandles,
+		ListModulesAll)
+	if err != nil {
+		return fmt.Errorf("failed to enum process modules - %w", err)
+	}
+
+	total := uint(actualHandlesReturned)
+
+	for i, moduleHandle := range moduleHandles[0:actualHandlesReturned] {
+		module, err := lookupModuleInfo(processHandle, moduleHandle)
+		if err != nil {
+			return fmt.Errorf("failed to lookup module info for handle: %v - %w",
+				moduleHandle, err)
+		}
+
+		err = iterFn(i, total, module)
+		if err != nil {
+			return fmt.Errorf("failed to iterate over modules - %w", err)
+		}
+	}
+
+	return nil
 }
 
 func lookupModuleInfo(processHandle syscall.Handle, moduleHandle syscall.Handle) (Module, error) {
@@ -80,23 +130,70 @@ func lookupModuleInfo(processHandle syscall.Handle, moduleHandle syscall.Handle)
 		Filepath: fileName,
 		Filename: filepath.Base(fileName),
 		BaseAddr: info.LpBaseOfDll,
+		EndAddr:  info.LpBaseOfDll + uintptr(info.SizeOfImage),
+		Size:     uint64(info.SizeOfImage),
 	}, nil
 }
 
-func EnumProcessModulesEx(hProcess syscall.Handle, lphModule []syscall.Handle) (uintptr, error) {
+func TotalEnumProcessModulesEx(hProcess syscall.Handle, dwFilterFlag uint32) (uint32, error) {
 	lpcbNeeded := uint32(0)
 
 	_, _, err := pEnumProcessModulesEx.Call(
-		uintptr(hProcess),
-		uintptr(unsafe.Pointer(&lphModule[0])),
-		uintptr(len(lphModule)),
-		uintptr(unsafe.Pointer(&lpcbNeeded)),
-		uintptr(0x03)) // 0x03 lists all 32-bit and 64-bit modules
-	if err.(syscall.Errno) != 0 {
+		uintptr(hProcess),                    // hProcess
+		uintptr(0),                           // lphModule
+		uintptr(0),                           // cb
+		uintptr(unsafe.Pointer(&lpcbNeeded)), // lpcbNeeded
+		uintptr(dwFilterFlag))
+	if isEnumProcessModulesExErrFatal(err) {
 		return 0, err
 	}
 
-	return uintptr(lpcbNeeded) / unsafe.Sizeof(syscall.Handle(0)), nil
+	return lpcbNeeded / uint32(unsafe.Sizeof(syscall.Handle(0))), nil
+}
+
+func EnumProcessModulesEx(hProcess syscall.Handle, lphModule []syscall.Handle, dwFilterFlag uint32) (uint32, error) {
+	lpcbNeeded := uint32(0)
+
+	moduleArraySizeBytes := uint32(uintptr(len(lphModule)) * unsafe.Sizeof(syscall.Handle(0)))
+
+	_, _, err := pEnumProcessModulesEx.Call(
+		uintptr(hProcess),                      // hProcess
+		uintptr(unsafe.Pointer(&lphModule[0])), // lphModule
+		uintptr(moduleArraySizeBytes),          // cb
+		uintptr(unsafe.Pointer(&lpcbNeeded)),   // lpcbNeeded
+		uintptr(dwFilterFlag))
+	if isEnumProcessModulesExErrFatal(err) {
+		return 0, err
+	}
+
+	return lpcbNeeded / uint32(unsafe.Sizeof(syscall.Handle(0))), nil
+}
+
+func isEnumProcessModulesExErrFatal(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	errno, isErrno := err.(syscall.Errno)
+	if !isErrno {
+		return true
+	}
+
+	switch errno {
+	case 0:
+		// OK.
+	case 299:
+		// Special case for:
+		// Only part of a ReadProcessMemory or
+		// WriteProcessMemory request was completed
+		//
+		// I have no idea why this is returned to us.
+		// Windoze.
+	default:
+		return true
+	}
+
+	return false
 }
 
 func GetModuleFilenameExW(hProcess syscall.Handle, hModule syscall.Handle) (string, error) {
@@ -107,7 +204,7 @@ func GetModuleFilenameExW(hProcess syscall.Handle, hModule syscall.Handle) (stri
 		uintptr(hModule),
 		uintptr(unsafe.Pointer(&lpFilename[0])),
 		uintptr(len(lpFilename)))
-	if err.(syscall.Errno) != 0 {
+	if isError(err) {
 		return "", err
 	}
 
@@ -126,9 +223,22 @@ func GetModuleInformation(hProcess syscall.Handle, hModule syscall.Handle, lpmod
 		uintptr(hModule),
 		uintptr(unsafe.Pointer(lpmodinfo)),
 		uintptr(uint32(unsafe.Sizeof(*lpmodinfo))))
-	if err.(syscall.Errno) != 0 {
+	if isError(err) {
 		return err
 	}
 
 	return nil
+}
+
+func isError(err error) bool {
+	if err == nil {
+		return false
+	}
+
+	errno, isErrno := err.(syscall.Errno)
+	if !isErrno {
+		return true
+	}
+
+	return errno != 0
 }
